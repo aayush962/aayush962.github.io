2294adf9ef1a196892a75359cad270f07824fc82
doubleA <aayush962@gmail.com>
2017-10-08 08:31:40 +0530

    push to github pages

---
+++ app.js
@@ -0,0 +1,18 @@
+// animations
+
+// function swingBoxUp(className){
+//   $('.'+className).animate({
+//     top: "+=5"
+//   }, 1000, "swing")
+// }
+//
+// $(document).ready(function(){
+//   setInterval(swingBoxUp('identity'), 1000)
+//
+//   setInterval(function(){
+//     $('.identity').animate({
+//       top: "-=5"
+//     }, 1000, "swing")
+//   }, 1000)
+//
+// })
